c9911af0744a6a0ad8ea6cf65c4b6a63f366dc05
(s) Chris Ogbeide <lk0751@lunet.lboro.ac.uk>
2026-03-03 15:15:34 +0000

    Changed the text component and added components, TextField and Button, within a Column component

--- lab_attempts/01/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ lab_attempts/01/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +36,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier =
+        modifier.padding(16.dp)) {
+        Text(
+            text = "Welcome to the weather app, $name!",
+            modifier = modifier
+        )
+        TextField(
+            value = "",
+            onValueChange = { /*To Do*/ }
+        )
+        Button(
+            onClick = { /* do */ }
+        )
+        {
+            Text(
+                text = "Refresh"
+            )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
